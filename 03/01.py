1f6600b6e31d6b931b86a8d2129f8d0c94596c7b
Roman Balandin <tinkerscript@yandex.ru>
2022-08-28 15:32:48 +0500

    add another solution for 03/01

--- 03/01.py
+++ 03/01.py
@@ -6,11 +6,15 @@
 my_list_1 = [2, 5, 8, 2, 12, 12, 4]
 my_list_2 = [2, 7, 12, 3]
 
-i = len(my_list_1) - 1
+# i = len(my_list_1) - 1
+#
+# while i >= 0:
+#     if my_list_1[i] in my_list_2:
+#         del my_list_1[i]
+#     i -= 1
 
-while i >= 0:
-    if my_list_1[i] in my_list_2:
-        del my_list_1[i]
-    i -= 1
+for number in my_list_1[:]:
+    if number in my_list_2:
+        my_list_1.remove(number)
 
 print(my_list_1)
